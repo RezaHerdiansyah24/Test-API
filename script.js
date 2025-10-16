c9c8c96bb08f8983155fb8fd58056f66702dcf78
Reza Herdiansyah <109779578+RezaHerdiansyah24@users.noreply.github.com>
2025-10-16 13:08:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const apiKey = "c07d254507d74604b84f3ea9c03cae7a"; // API key NewsAPI.org milik kamu
+const apiKey1 = "de1eab907c7748f18b22bb50ac948327"; // API key NewsAPI.org milik kamu
+const apiKey = "c07d254507d74604b84f3ea9c03cae7a";
 const newsContainer = document.getElementById("newsContainer");
+
+// Deteksi apakah dijalankan dari file://
+const isLocalFile = window.location.protocol === "file:";
+
+// Jika bukan localhost, pakai proxy (hanya untuk testing)
+const proxy = isLocalFile ? "https://cors-anywhere.herokuapp.com/" : "";
 
 async function getNews(category = "general") {
   newsContainer.innerHTML = "<p>Loading berita...</p>";
+  const url = `${proxy}https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=de1eab907c7748f18b22bb50ac948327`;
 
   try {
-    const res = await fetch(
-      `https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=de1eab907c7748f18b22bb50ac948327`
-    );
-    const data = await res.json();
+    const res = await fetch(url);
 
+    const data = await res.json();
+    console.log(data);
     if (data.status !== "ok") {
       throw new Error(data.message || "Gagal mengambil berita");
     }
@@ -22,27 +29,27 @@ async function getNews(category = "general") {
     newsContainer.innerHTML = data.articles
       .map(
         (article) => `
-            <div class="news-card">
-                <img src="${
-                  article.urlToImage ||
-                  "https://via.placeholder.com/300x180?text=No+Image"
-                }" alt="news image">
-                <div class="content">
-                <h3>${article.title || "Tanpa Judul"}</h3>
-                <p>${article.description || "Tidak ada deskripsi."}</p>
-                <a href="${article.url}" target="_blank">Baca Selengkapnya</a>
-                </div>
-            </div>
-            `
+      <div class="news-card">
+        <img src="${
+          article.urlToImage ||
+          "https://via.placeholder.com/300x180?text=No+Image"
+        }" alt="news image">
+        <div class="content">
+          <h3>${article.title || "Tanpa Judul"}</h3>
+          <p>${article.description || "Tidak ada deskripsi."}</p>
+          <a href="${article.url}" target="_blank">Baca Selengkapnya</a>
+        </div>
+      </div>
+    `
       )
       .join("");
   } catch (err) {
     console.error(err);
-    newsContainer.innerHTML = `<p>Terjadi kesalahan saat mengambil berita: ${err.message}</p>`;
+    newsContainer.innerHTML = `<p>Terjadi kesalahan: ${err.message}</p>`;
   }
 }
 
-// Tampilkan berita umum saat halaman pertama dibuka
+// Tampilkan berita umum saat pertama
 getNews();
 
 // Navbar event listeners
